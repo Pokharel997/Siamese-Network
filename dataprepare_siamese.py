c604fc6d39b7a085b6daf793e1c5a8b410e7f897
dipesh-commits <dipesh9393@gmail.com>
2020-03-26 14:40:18 +0545

    Change in code

--- dataprepare_siamese.py
+++ dataprepare_siamese.py
@@ -24,9 +24,11 @@ def load_image(path):
 	current_y = 0
 	X =[]
 	Y =[]
+	lang_dict = {}
 
 	for img_folder in os.listdir(path):
 		image_folder_path = os.path.join(path,img_folder)
+		lang_dict[img_folder] = [current_y,None]
 		for img in os.listdir(image_folder_path):
 			image_path = os.path.join(image_folder_path,img)
 			image = cv2.imread(image_path)
@@ -34,21 +36,23 @@ def load_image(path):
 			X.append(np.stack(image))
 			Y.append(current_y)
 		current_y+=1
+		lang_dict[img_folder][1] = current_y - 1
 	X = np.stack(X)
 	Y = np.vstack(Y)
 	# Y = le.fit_transform(classes)
 	# Y = to_categorical(Y,5)
-	return X,Y
+	return X,Y,lang_dict
 
 
 
-X,y  = load_image(valid_path)
+X,y,c = load_image(valid_path)
 print(X.shape)
+print(c)
 
 
 
-# with open('test.pickle','wb') as f:
-# 	pickle.dump((X,y),f)
+with open('valid.pickle','wb') as f:
+	pickle.dump((X,c),f)
 
 	
 
