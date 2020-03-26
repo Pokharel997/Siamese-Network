c5940c42a1385e14f482b4b076b6811500819809
dipesh-commits <dipesh9393@gmail.com>
2020-03-26 13:02:52 +0545

    Shape error debugged

--- dataprepare_siamese.py
+++ dataprepare_siamese.py
@@ -5,42 +5,49 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+import cv2
 
 
 data_path = 'data'
 train_path = 'data/train'
-valid_path = 'data/valid'
+valid_path = 'data/val'
 
-# save_path= 'face_detect/'
+save_path= 'face_detect/'
 
 
-le = LabelEncoder()
+# le = LabelEncoder()
+
+
 
 
 def load_image(path):
-	X = []
-	classes = []
+	current_y = 0
+	X =[]
+	Y =[]
 
 	for img_folder in os.listdir(path):
 		image_folder_path = os.path.join(path,img_folder)
 		for img in os.listdir(image_folder_path):
 			image_path = os.path.join(image_folder_path,img)
-			image = Image.open(image_path)
-			pixels = np.asarray(image)
-			classes.append(img_folder)
-			X.append(np.stack(pixels))
-
+			image = cv2.imread(image_path)
+			image = cv2.resize(image,(200,200))
+			X.append(np.stack(image))
+			Y.append(current_y)
+		current_y+=1
+	X = np.stack(X)
+	Y = np.vstack(Y)
 	# Y = le.fit_transform(classes)
 	# Y = to_categorical(Y,5)
-	return X,classes
+	return X,Y
 
 
 
-X,y  = load_image(train_path)
+X,y  = load_image(valid_path)
+print(X.shape)
 
-print(X,y)
 
-# with open(os.path.join(save_path,"train.pickle"),"wb") as f:
+
+# with open('test.pickle','wb') as f:
 # 	pickle.dump((X,y),f)
 
 	
